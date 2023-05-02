aa97430a34ee14562dedec9438ae9e1ff0c51b29
edohan <edohan0117@gmail.com>
2023-05-02 16:15:02 +0900

    //first commit

---
+++ src/main/java/com/edohan/root/board/controller/BoardController.java
@@ -0,0 +1,19 @@
+package com.edohan.root.board.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    public Logger log = LoggerFactory.getLogger(BoardController.class);
+    @GetMapping("/list")
+    public String boardList(){
+        log.info("Board/List 로 들어옴");
+        return "board/list";
+    }
+}
